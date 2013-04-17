64fb6525ab73f5a67e5db810059b97cc08198db3
Zach Fry <fryz@rpi.edu>
2013-04-17 20:55:19 +0000

    trying to grab boston tweets

--- twitter_crawler/scripts/pastCrawler.py
+++ twitter_crawler/scripts/pastCrawler.py
@@ -1,6 +1,6 @@
 '''
 @author Zach Fry (fryz)
-@date 2/10/2013
+@date 4/17/2013
 '''
 
 import os
@@ -29,7 +29,7 @@ hashtags = ['#SMEM',
 datestr = strftime('%m-%d-%Y', localtime())
 timestr = strftime('%H:%M', localtime())
 root_url = 'http://search.twitter.com/search.json'
-root_path = '/home/fryz/twitter_crawler/results/'
+root_path = '/home/fryz/FirstResponder/twitter_crawler/results/'
 
 def requestURL(url, path, page):
     #print url
@@ -37,10 +37,11 @@ def requestURL(url, path, page):
         req = urllib2.Request(url)
         response = urllib2.urlopen(req)
         #print response.read()
-    except:
+    except Exception as e:
         f = open('error.txt', 'a')
         f.write('%s-%s\t%s\n'%(datestr,timestr,url))
         f.close()
+        print type(e), e.args, e
     else:
         json_obj = json.loads(response.read())
 
@@ -49,9 +50,9 @@ def requestURL(url, path, page):
 
         try:
             next_url = json_obj['next_page']
-            requestURL(root_url+next_url, path, page+1)
+            return requestURL(root_url+next_url, path, page+1)
         except KeyError:
-           pass
+            return page
         finally:
             if len(json_obj['results']) > 0:
                 f = open(path+'%s-%s-%s.json'%(datestr,timestr,page),'w')
@@ -62,31 +63,43 @@ def requestURL(url, path, page):
         f = open(path+'maxid.txt', 'w')
         f.write(str(json_obj['max_id']))
         f.close()
+    
+    return -1
 
 def main():
     day = 'Boston/'
     global root_path
     root_path = root_path + day
 
-    for tag in hashtags:
-        path = root_path + tag.strip('#')+'/'
-        if os.path.isdir(path) is False or os.path.exists(path) is False:
-            os.mkdir(path)
 
-        stripped_tag = '%23' + tag.strip('#')
-        url = root_url
-        url += '?result_type=recent&rpp=100&lang=en&rpp=100&include_entities=true&q='
-        url += stripped_tag
+    max_pages = {}
+    for day in range(14,18):
+        for tag in hashtags:
+            if tag not in max_pages: max_pages[tag] = 1
+            print 'extracting hashtag %s'%tag
+            path = root_path + tag.strip('#')+'/'
+            if os.path.isdir(path) is False or os.path.exists(path) is False:
+                os.mkdir(path)
 
-        try:
-            f = open(path+'maxid.txt', 'r')
-            max_id = f.readline().strip()
-            f.close()
-            url += '&since_id=%s'%(max_id)
-        except:
-            pass
+            stripped_tag = '%23' + tag.strip('#')
+            url = root_url
+            url += '?result_type=recent&lang=en&rpp=100&include_entities=true&until=2013-04-%s&q='%day
+            url += stripped_tag
 
-        requestURL(url, path, 1)
+            page = max_pages[tag]
+            while 1:
+                tmp_url = url
+                try:
+                    f = open(path+'maxid.txt', 'r')
+                    max_id = f.readline().strip()
+                    f.close()
+                    tmp_url += '&since_id=%s'%(max_id)
+                except:
+                    pass
 
+                p = requestURL(tmp_url, path, max_pages[tag]) 
+                if p > 0: break
+                else: max_pages[tag] = p+1
 
 main()
+
