b180228c372d9faa13ea804fdd664127877f0e68
DeskOS <desktop.os.rpi@gmail.com>
2013-05-15 13:06:56 +0000

    Adding dashboard to source control

---
+++ static_demo/mallet_output_wrapper.js
@@ -0,0 +1,11 @@
+var output = "0	0.00753	socialmedia smm marketing twitter facebook social business fb pr wx seo uswx forecast linkedin infographic socbiz google instagram smallbiz #\
+1	0.00569	em hsem bfp love oh gee itshardformeto me happy firstresponder up smile cute like friends bellletstalk my bars #\
+2	0.00102	mgmt innovation mktg tech business oi wx health strategy nook kindle ipad ezines magazines roc em articles excellence growth #\
+3	0.0078	smem smemchat hospitality em earthquake socialmedia canvost usgs femathinktank everypost coems hsem embarrassing gov vost tips msgu smilecon smansaproudigy #\
+4	0.00299	wx nemo blizzard snow smem us news newengland travel boston nywx nyc mawx weather northeast onstorm emcomm kpop ma #\
+5	0.00141	emilyowensmd smallville wx tornado news us mswx ms alwx al severe ohwx toledo cic findlay ohio onnml emblem oc #\
+6	0.00137	smem uasi hsem nema dhs iaem fema jobs twitter em whs ohs disaster safetytraining disasters uk worksafety wales employment #\
+7	0.00672	gov egov opengov opendata wx va vawx digitalgov ogov socialmedia localgov citizenville newschallenge plonegov tech bungeni bigdata byod cto #\
+8	0.00263	wx news weather blizzard media ecommerce facebook smo serp linkbuilding tornado mesodiscussion weathe gawx nyc journalism radar atl severe #\
+9	0.00646	wx wv wvwx ncwx txwx firewx winter severe omgsnow prayforhattiesburg uksnow yeahmann safetytip weatherwoodie itgonsnow chevy bringiton backyard captainplanet ";
+
